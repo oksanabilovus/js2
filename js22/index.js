32fae7eceefdff123644f1374eb74cf8d1e33f36
Oksana Bilovus <o.top@ukr.net>
2016-11-07 21:33:33 +0000

    c

---
+++ js22/index.js
@@ -0,0 +1,24 @@
+var a = Number(prompt('Input a'));
+var b = Number(prompt('Input b'));
+var c = Number(prompt('Input c'));
+var d = b * b + 4 * a * c;
+var x1;
+var x2;
+if (d < 0) {
+    return 'No interchanges';
+  } else {
+if (d = 0)
+    return 'result';
+  } else {
+if (d > 0)
+    return 'result';
+  }
+var result = (-b + Math.sqrt(d)) / 2 * a;
+var result = (-b - Math.sqrt(d)) / 2 * a;
+document.write(result);
+//function sumArgs(a, b, c) {
+//    var result = a + b + c;
+//    return result;
+//}
+//result = Math.sqrt(25);
+//document.write(result);
