159c6ef7d5ce673e2a0bbce4a62c3e107b63b189
Oksana Bilovus <o.top@ukr.net>
2016-11-09 16:24:39 +0000

    v

--- js22/index.js
+++ js22/index.js
@@ -1,18 +1,27 @@
 var a = Number(prompt('Input a'));
 var b = Number(prompt('Input b'));
 var c = Number(prompt('Input c'));
-var d = b * b - 4 * a * c;
-var dx1 = - b + Math.sqrt(d);
-var dx2 = - b - Math.sqrt(d);
-if (d < 0) {
-    return 'No real roots exist';
-  } else {
-   if (d > 0)
-   return 'result';
-  }
-var x1 = dx1 / (2 * a);
-var x2 = dx2 / (2 * a);
-var result1 = x1;
-var result2 = x2;
-document.write(result1, result2);
+
+
+function calculate(a, b, c) {
+
+var dis = b * b - 4 * a * c;
+var a2 = 2 * a;
+
+     if(dis<0){
+        document.getElementById('Equation').innerHTML='No real roots exist since Discriminant < 0 !<br />D = ' + dis + ' <br />The Equation = ' + a + 'x&#178; + ' + b + 'x + ' + c + '<br />';
+        document.getElementById('x1').innerHTML='&nbsp; ';
+        document.getElementById('x2').innerHTML='&nbsp; ';
+        }
+     else{
+        var dis_sqrt=Math.sqrt(dis);
+        var x1=-b+dis_sqrt;
+        var x1=x1/a2;
+        var x2=-b-dis_sqrt;
+        var x2=x2/a2;
+        document.getElementById('Equation').innerHTML=" Квадратне рівняння = " + a + "x&#178; + " + b + "x + " + c + "<br />";
+        document.getElementById('x1').innerHTML=' (x1) = ' + x1;
+        document.getElementById('x2').innerHTML=' (x2) = ' + x2;
+        }
+    }
 
